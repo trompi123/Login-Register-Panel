fed048f0969cd055d7c4e0c2ecdaf3887b27ff43
Mario <marioperdiguero37@gmail.com>
2020-11-24 13:45:19 +0100

    Interface implementation
    Creating the main JFrame and the two buttons

---
+++ src/jframes/Menu.java
@@ -0,0 +1,49 @@
+package jframes;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Menu extends JFrame {
+
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Menu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 470);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnLogIn.setBounds(32, 21, 264, 96);
+		contentPane.add(btnLogIn);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.setBounds(32, 300, 264, 96);
+		contentPane.add(btnRegister);
+	}
+}
